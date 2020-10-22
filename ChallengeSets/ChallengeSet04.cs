306691586d66273fc465e1e71f9c447e5ed94318
Olakunle E Neye <emmanuelneye@gmail.com>
2020-10-22 12:14:03 -0400

    ChallengeSet 4 - Solved the Factorial method and solved for negative factorial edge case

--- ChallengeSets/ChallengeSet04.cs
+++ ChallengeSets/ChallengeSet04.cs
@@ -71,7 +71,16 @@ namespace ChallengeSets
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            if (number < 0) throw new ArgumentOutOfRangeException();
+            if (number == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Factorial(number - 1);
+            }
+            
         }
     }
 }

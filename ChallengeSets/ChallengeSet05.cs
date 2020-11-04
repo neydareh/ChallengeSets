19d5c987c5960735e9a5a2386546eb0d86a5bc9a
Olakunle E Neye <emmanuelneye@gmail.com>
2020-11-04 17:49:20 -0500

    ChallengeSet05 - Solved the GetNextNumberDivisibleByN method

--- ChallengeSets/ChallengeSet05.cs
+++ ChallengeSets/ChallengeSet05.cs
@@ -7,7 +7,9 @@ namespace ChallengeSets
     {
         public int GetNextNumberDivisibleByN(int startNumber, int n)
         {
-            throw new NotImplementedException();
+            // if startNumber is divisible by n, then multiply startNumber by n
+            // if startNumber is not divisible by n, then divide startNumber by n, multiply the result by n, then add it to n to get the next term
+            return (startNumber % n ==0) ? (startNumber + n) : ((startNumber / n) * n) + n;
         }
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)

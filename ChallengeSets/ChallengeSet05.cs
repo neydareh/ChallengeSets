5d4658f2b68b019c3aa1bc3c3e21676ab28d44f9
Olakunle E Neye <emmanuelneye@gmail.com>
2020-11-04 18:13:15 -0500

    ChallengeSet05 - Solved the ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED method

--- ChallengeSets/ChallengeSet05.cs
+++ ChallengeSets/ChallengeSet05.cs
@@ -14,12 +14,34 @@ namespace ChallengeSets
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
-            throw new NotImplementedException();
+            // Check to see if business has Revenue, if it has revenue, skip
+            // else change business name with no revenue to CLOSED.
+            foreach (var business in businesses)
+            {
+                if(business.TotalRevenue == 0)
+                {
+                    business.Name = "CLOSED";
+                }
+            }
         }
 
         public bool IsAscendingOrder(int[] numbers)
         {
-            throw new NotImplementedException();
+            // Check if number is in ascending order 
+            // (if i is less than i + 1, and i + 1 is less than i + 2, i + n -1 is less than i + n)
+            int measure = 0;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] > numbers[i + 1])
+                {
+                    measure += 1;
+                }
+            }
+            if (measure > 0)
+            {
+                return true;
+            }
+            else return false;
         }
 
         public int SumElementsThatFollowAnEven(int[] numbers)
